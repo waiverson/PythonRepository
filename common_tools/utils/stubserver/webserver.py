5db8bdb6b2b37c3061c1e5a6a22e73326657238e
waiverson <237476161@qq.com>
2016-04-19 13:48:25 +0800

    add stubserver for unit test

--- common_tools/utils/stubserver/webserver.py
+++ common_tools/utils/stubserver/webserver.py
@@ -15,6 +15,31 @@ else:
     from urllib.request import urlopen
 
 
+"""
+#example:
+  from unittest import TestCase
+  from stubserver import StubServer
+
+  class WebTest(TestCase):
+
+      def setUp(self):
+          self.server = StubServer(8998)
+          self.server.run()
+
+      def tearDown(self):
+          self.server.stop()
+          # implicitly calls verify on stop
+
+      def test_put_with_capture(self):
+          capture = {}
+          self.server.expect(method="PUT", url="/address/\d+$", data_capture=capture) \
+                     .and_return(reply_code=201)
+
+          # do stuff here
+          captured = eval(capture["body"])
+          self.assertEquals("world", captured["hello"])
+"""
+
 class StoppableHTTPServer(BaseHTTPServer.HTTPServer):
     """
     Python 2.5 HTTPServer does not close down properly when calling server_close.
