2b4ca0a2e8c25591f6b788f0b9462ba1d62ab01a
waiverson <237476161@qq.com>
2016-05-17 10:50:39 +0800

    add rabbitmq test file

---
+++ common_tools/utils/rabbitmq_test/server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+__author__ = 'xyc'
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='test_queue')
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+def on_request(ch, method, props, body):
+    n = int(body)
+    print " [.] fib(%s)" % (n,)
+    response = fib(n)
+
+    ch.basic_public(exchange='',
+                    routing_key=props.reply_to,
+                    properties=pika.BasicProperties(correlation_id=props.correlation_id),
+                    body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='test_queue')
+
+print '[x] Awating Test requests'
+channel.start_consuming()
