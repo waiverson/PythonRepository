e43d8645bfe97eb53f3ecfe54e272cfbb2b53cd4
waiverson <237476161@qq.com>
2016-05-17 15:06:11 +0800

    add rabbitmq test file

---
+++ common_tools/utils/rabbitmq_test/emit_log.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import pika, sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs',
+                            type='direct')
+
+severity = sys.argv[1] if len(sys.argv) > 1 else 'info'
+message = ' '.join(sys.argv[2:]) or "Hello World!"
+
+channel.basic_publish(exchange='direct_logs',
+                      routing_key=severity,
+                      body=message,
+                      )
+print " [x] Sent %r:%r" % (severity, message)
+
+connection.close()
